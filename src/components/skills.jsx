2d9070211f4457b3e0dd47695c765e7cb3bd7b26
Twan <88901@glr.nl>
2024-10-05 20:31:51 +0200

    kleine upd

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,74 +1,73 @@
 // src/components/Hero.js
-import "../css/plans.css";
+import "../css/skills.css";
 
 export default function Skills() {
   return (
     <section className="plans">
       <div className="pricing-plans">
-        <div className="plan basic-plan">
+        <div className="plan">
           <h3>Frameworks & Libraries</h3>
           <p className="description">Frameworks waar ik ervaring mee heb.</p>
           <ul className="features">
             <li>
-              <i className="bx bx-check"></i> React
+              <i className="devicon-react-original colored"></i> React
             </li>
             <li>
-              <i className="bx bx-check"></i> Laravel
+              <i className="devicon-laravel-plain colored"></i> Laravel
             </li>
             <li>
-              <i className="bx bx-check"></i> NextJs
+              <i class="devicon-nextjs-original-wordmark"></i>
+              NextJs
             </li>
             <li>
-              <i className="bx bx-check"></i> Svelte
+              <i className="devicon-svelte-plain colored"></i> Svelte
             </li>
             <li>
-              <i className="bx bx-check"></i> Electron
+              <i className="devicon-electron-original colored"></i> Electron
             </li>
           </ul>
         </div>
 
-        <div className="plan business-plan">
-          <h3>
-            Talen & Tech
-          </h3>
+        <div className="plan main-skills">
+          <h3>Talen & Tech</h3>
           <p className="description">Talen waar ik ervaring mee heb.</p>
           <ul className="features">
             <li>
-              <i className="bx bx-check"></i> Javascript
+              <i className="devicon-javascript-plain colored"></i> Javascript
             </li>
             <li>
-              <i className="bx bx-check"></i> PHP
+              <i className="devicon-php-plain colored"></i> PHP
             </li>
             <li>
-              <i className="bx bx-check"></i> HTML5
+              <i className="devicon-html5-plain colored"></i> HTML5
             </li>
             <li>
-              <i className="bx bx-check"></i> CSS5
+              <i className="devicon-css3-plain colored"></i> CSS5
             </li>
             <li>
-              <i className="bx bx-check"></i> SASS
+              <i className="devicon-sass-original colored"></i> SASS
             </li>
             <li>
-              <i className="bx bx-check"></i> NodeJS
+              <i className="devicon-nodejs-plain colored"></i> NodeJS
             </li>
           </ul>
         </div>
 
-        <div className="plan enterprise-plan">
+        <div className="plan">
           <h3>Databases</h3>
-          <p className="description">Databases waar ik ervaring mee heb</p>
+          <p className="description">Databases waar ik ervaring mee heb.</p>
           <ul className="features">
             <li>
-              <i className="bx bx-check"></i> MySql
+              <i className="devicon-mysql-plain colored"></i> MySql
             </li>
             <li>
-              <i className="bx bx-check"></i> Firebase
+              <i className="devicon-firebase-plain colored"></i> Firebase
             </li>
             <li>
-              <i className="bx bx-check"></i> MariaDB
+              <i className="devicon-mariadb-plain colored"></i> MariaDB
             </li>
             <li>
-              <i className="bx bx-check"></i> SQLite
+              <i className="devicon-sqlite-plain colored"></i> SQLite
             </li>
           </ul>
         </div>
