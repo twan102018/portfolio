95c146f9556bec287f976d82a966114faf1ba155
Twan <71022124+twan102018@users.noreply.github.com>
2024-10-10 14:05:35 +0200

    typewriter animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
   return (
     <section className="hero">
       <div className="hero-content" onMouseEnter={textEnter} onMouseLeave={textLeave}>
-        <h2 className="hero-title"><span className='span-hero'>I'm an 18</span> year old software developer</h2>
+        <h2 className="hero-title"><span className='span-hero'>I'm an 18 year old software developer </span></h2>
         <h3 className="hero-subtitle">Hey, hallo mijn naam is Twan en ik werk met pixels</h3>
         <div className="button-group">
           <button className="btn">Projecten</button>
